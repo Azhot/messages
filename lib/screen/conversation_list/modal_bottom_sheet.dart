d17c60fad4b68aa71b514ebe8198b91d998eac8e
Azhot <francois.jouvelot@gmail.com>
2021-12-31 16:31:50 +0100

    Modal bottom sheet in its own file

---
+++ lib/screen/conversation_list/modal_bottom_sheet.dart
@@ -0,0 +1,68 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+import 'package:messages/dependency_injection/injection.dart';
+import 'package:messages/dependency_injection/use_case/create_conversation.dart';
+import 'package:messages/dependency_injection/use_case/get_users.dart';
+import 'package:messages/model/user.dart';
+import 'package:messages/shared/constants.dart';
+import 'package:messages/shared/strings.dart';
+import 'package:messages/shared/widget/loading.dart';
+import 'package:messages/shared/widget/send_text_field.dart';
+import 'package:messages/shared/widget/something_went_wrong.dart';
+
+class ModalBottomSheet extends StatelessWidget {
+  // variables
+  final TextEditingController controller;
+
+  // constructors
+  const ModalBottomSheet(this.controller, {Key? key}) : super(key: key);
+
+  // overrides
+  @override
+  Widget build(BuildContext context) => Column(
+        children: [
+          conversationTitleField(context),
+          Expanded(child: usersStreamBuilder(context)),
+        ],
+      );
+
+  // functions
+  Widget conversationTitleField(BuildContext context) => SendTextField(
+        hintText: Strings.newConversationTitle,
+        maxLines: 1,
+        onSend: (String text) => {
+          inject<CreateConversation>().execute(title: text),
+          Navigator.pop(context)
+        },
+      );
+
+  StreamBuilder<QuerySnapshot> usersStreamBuilder(BuildContext context) =>
+      StreamBuilder<QuerySnapshot>(
+        stream: inject<GetUsers>().execute(),
+        builder: (context, snapshot) =>
+            snapshot.connectionState == ConnectionState.waiting
+                ? const Loading()
+                : snapshot.hasError
+                    ? const SomethingWentWrong()
+                    : snapshot.data != null
+                        ? usersListView(snapshot.data!.docs)
+                        : const SomethingWentWrong(),
+      );
+
+  Widget usersListView(List<QueryDocumentSnapshot> docs) => Material(
+      color: Constants.primaryColorLight,
+      child: ListView(children: docs.map((doc) => userTile(doc)).toList()));
+
+  ListTile userTile(QueryDocumentSnapshot doc) => ListTile(
+        leading: const SizedBox(
+            height: double.infinity,
+            child: Icon(
+              Icons.person_add,
+              color: Constants.secondaryColorLight,
+              size: 32,
+            )),
+        title: Text(doc[User.nameField]),
+        subtitle: Text(doc[User.emailField]),
+        onTap: () => {},
+      );
+}
